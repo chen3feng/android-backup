d00fce5949ceaf13c906835b473ad01ffb499b91
chen3feng <chen3feng@gmail.com>
2025-07-29 01:40:36 +0800

    Fix the directory layout and ensure the pull command works correctly.

--- adbsync/adb.py
+++ adbsync/adb.py
@@ -19,7 +19,14 @@ class ADB():
         return self.run(['shell'] + [cmd])
 
     def pull(self, root, source_dir, target_dir, old_backup_dir, exclude_file):
-        cmd = ['pull', os.path.join(root, source_dir), os.path.join(target_dir, source_dir)]
+        # Construct the pull command
+        # Note the use of '-a' to preserve file attributes
+        # The target directory is the parent directory of the source_dir to ensure the structure is maintained.
+        # `adb pull source_dir/ target_dir` way does not work as expected because it creates a new subdirectory in target_dir if the target_dir already exist.
+        dest_dir = os.path.dirname(os.path.join(target_dir, source_dir))
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        cmd = ['pull', '-a', os.path.join(root, source_dir), dest_dir]
         return self.run(cmd)
 
 class RemoteFiles:
