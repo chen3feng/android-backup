f459622639268600e53c4990111ed514c8648476
CHEN Feng <chen3feng@gmail.com>
2025-07-31 13:31:46 +0800

    Rewrite with python (#3)
    * Using python to implement the ADB sync tool
    * Support incremental pull
    * Support exclude-file
    * Support running the code on Windows system

---
+++ adbsync/adb.py
@@ -0,0 +1,161 @@
+import itertools
+import os
+import posixpath
+import shlex
+import subprocess
+import typing
+
+import pathspec
+
+DirScanResultType = typing.Tuple[typing.Dict[str, int], typing.Dict[str, typing.Tuple[int, float]]]
+
+def scan_local_dir(root: str, subdir: str = "", filter: pathspec.PathSpec = None) -> DirScanResultType:
+    dirs, files = {}, {}
+    base = os.path.join(root, subdir)
+    for dirpath, _, filenames in os.walk(base):
+        rel_dirpath = os.path.relpath(dirpath, root).replace("\\", "/")
+        try:
+            if not filter.match_file(rel_dirpath):
+                stat = os.stat(dirpath)
+                dirs[rel_dirpath] = stat.st_mtime
+            # else:
+            #     print(f'Exclude {rel_dirpath}')
+        except FileNotFoundError:
+            # 目录可能在遍历过程中被删除
+            pass
+        for name in filenames:
+            full_path = os.path.join(dirpath, name)
+            rel_path = os.path.relpath(full_path, root).replace("\\", "/")
+            if filter.match_file(rel_path):
+                # print(f'Exclude {rel_path}')
+                continue
+            try:
+                stat = os.stat(full_path)
+                files[rel_path] = (stat.st_size, stat.st_mtime)
+            except FileNotFoundError:
+                # 文件可能在遍历过程中被删除
+                continue
+    return dirs, files
+
+
+def load_exclude_file(exclude_file: str) -> pathspec.PathSpec:
+    if not exclude_file:
+        return pathspec.PathSpec()
+    with open(exclude_file, 'r') as fh:
+        spec = pathspec.PathSpec.from_lines('gitwildmatch', fh)
+        return spec
+
+
+class ADB():
+    def __init__(self, adb: str, device: str = None):
+        self.adb = adb
+        self.device = device
+
+    def run(self, cmd: typing.List[str], *args, **kwargs) -> int:
+        full_cmd = self.extend_cmd(cmd)
+        # print(f"Running command: {' '.join(full_cmd)}")
+        return subprocess.call(full_cmd, *args, **kwargs)
+
+    def check_output(self, cmd: typing.List[str], *args, **kwargs) -> str:
+        full_cmd = self.extend_cmd(cmd)
+        # print(f"Running command: {' '.join(full_cmd)}")
+        return subprocess.check_output(full_cmd, text=True, *args, **kwargs)
+
+    def extend_cmd(self, cmd: typing.List[str]) -> typing.List[str]:
+        full_cmd = [self.adb]
+        if self.device:
+            full_cmd.extend(['-s', self.device])
+        full_cmd.extend(cmd)
+        return full_cmd
+
+    def shell(self, cmd) -> int:
+        return self.run(['shell'] + [cmd])
+
+    def pull(self, root, source_dir, target_dir, old_backup_dir, exclude_file):
+        print(f'Pulling {source_dir}...')
+        filter = load_exclude_file(exclude_file)
+        remote_dirs, remote_files = self.scan_remote_dir(root, source_dir, filter)
+        local_dirs, local_files = scan_local_dir(target_dir, source_dir, filter)
+        pull_files = self.get_pull_files(remote_files, local_files)
+        # self.pull_dir(root, source_dir, target_dir, old_backup_dir, filter)
+        self.pull_files(root, pull_files, target_dir)
+
+    def scan_remote_dir(self, root, source_dir, filter):
+        # TODO: call find only once
+        find_cmd = ['find', posixpath.join(root, source_dir), '-type', 'f', '-printf', '%s %T@ %P\n']
+        cmd = ['shell', " ".join(shlex.quote(a) for a in find_cmd)]
+        output = self.check_output(cmd)
+        files = self.parse_find_file_outout(root, source_dir, output, filter)
+
+        find_cmd = ['find', posixpath.join(root, source_dir), '-type', 'd', '-printf', '%T@ %P\n']
+        cmd = ['shell', " ".join(shlex.quote(a) for a in find_cmd)]
+        output = self.check_output(cmd)
+        dirs = self.parse_find_dir_output(root, source_dir, output, filter)
+
+        return dirs, files
+
+    def print_files(self, files):
+        for path, (size, mtime) in itertools.islice(files.items(), 10):
+            print(f"{path}: {size} bytes, modified at {mtime}")
+
+    def parse_find_dir_output(self, root, source_dir, output, filter):
+        result = {}
+        for line in output.splitlines():
+            parts = line.strip().split(" ", 1)
+            if len(parts) < 2:
+                # The source_dir itself
+                continue
+            mtime = float(parts[0])
+            path = parts[1]
+            if not filter.match_file(path):
+                result[posixpath.join(source_dir, path)] = mtime
+        return result
+
+    def parse_find_file_outout(self, root, source_dir, output, filter):
+        result = {}
+        for line in output.splitlines():
+            parts = line.strip().split(" ", 2)
+            if len(parts) < 3:
+                raise ValueError(f"Line not in expected format: {line}")
+            size = int(parts[0])
+            mtime = float(parts[1])
+            path = parts[2]
+            if not filter.match_file(path):
+                result[posixpath.join(source_dir, path)] = (size, mtime)
+        return result
+
+    def get_pull_files(self, remote_files, local_files):
+        result = []
+        for path, (rf_size, rf_mtime) in remote_files.items():
+            lf = local_files.get(path)
+            if not lf:
+                result.append(path)
+                continue
+            lf_size, lf_mtime = lf
+            if rf_size != lf_size or abs(rf_mtime - lf_mtime) > 2:
+                result.append(path)
+        return result
+
+    def remove_excluded(self, root, source_dir, filter: pathspec.PathSpec) -> int:
+        pass
+
+    def pull_dir(self, root, source_dir, target_dir, old_backup_dir, filter):
+        # Construct the pull command
+        # Note the use of '-a' to preserve file attributes
+        # The target directory is the parent directory of the source_dir to ensure the structure is maintained.
+        # `adb pull source_dir/ target_dir` way does not work as expected because it creates a new subdirectory in target_dir if the target_dir already exist.
+        dest_dir = os.path.dirname(os.path.join(target_dir, source_dir))
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        cmd = ['pull', '-a', posixpath.join(root, source_dir), dest_dir]
+        self.run(cmd)
+        self.remove_excluded(target_dir, source_dir, filter)
+
+    def pull_files(self, root, files, target_dir):
+        for f in files:
+            ff = posixpath.join(root, f)
+            dest_dir = os.path.join(target_dir, os.path.dirname(f))
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir)
+            cmd = ['pull', '-a', ff, dest_dir]
+            self.run(cmd)
---
+++ backup.py
@@ -0,0 +1,111 @@
+import argparse
+import importlib.util
+from importlib.machinery import SourceFileLoader
+import os
+import posixpath 
+import subprocess
+import types
+import typing
+
+import adbsync
+
+
+def main():
+    config = load_config(os.path.join(os.path.dirname(__file__), 'global.conf'))
+    if not config:
+        print("No global configuration found.")
+        return
+    adb_path = config.ADB_PATH
+    if not adb_path:
+        adb_path = find_adb_path()
+    if not adb_path:
+        print("ADB_PATH not found in configuration or environment.")
+        return
+    
+    print(f"Using adb path: {adb_path}")
+
+    serial = find_device_serial(adb_path)
+    if not serial:
+        print("No device connected.")
+        return
+
+    print(f"Find device serial: {serial}")
+
+    device_config_path = os.path.join(os.path.dirname(__file__), 'devices', f'{serial}.conf')
+    if not os.path.exists(device_config_path):
+        print(f"No configuration {device_config_path} found.")
+        return
+    device_config = load_config(device_config_path)
+    if not device_config:
+        print(f"Failed to load device configuration from {device_config_path}.")
+        return
+    print(f"Loaded device configuration: {device_config.DEVICE_NAME}")
+
+    for root, source_dir in device_config.INCLUDE_DIRS:
+        print(f"Pulling from {root}/{source_dir} to {posixpath.join(config.BACKUP_BASE_DIR, device_config.DEVICE_NAME)}")
+        adbsync.pull(
+            adb_path=adb_path,
+            serial=serial,
+            root=root,
+            source_dir=source_dir,
+            exclude_file=config.DEFAULT_EXCLUDE_FILE,
+            target_dir=posixpath.join(config.BACKUP_BASE_DIR, device_config.DEVICE_NAME),
+            old_backup_dir=None
+        )
+
+
+def load_config(config_file)-> typing.Optional[types.ModuleType]:
+    """
+    Load global configuration from a file.
+    """
+    try:
+        config = SourceFileLoader("config_data", config_file).load_module()
+        return config
+    except Exception as e:
+        print(f"Error loading configuration from {config_file}: {e}")
+        return None
+
+
+def find_adb_path() -> str:
+    """
+    Find the adb executable path.
+    This function should be implemented to locate the adb executable.
+    """
+    if os.name == 'nt':
+        if subprocess.call(['where', 'adb'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0:
+            adb_path = subprocess.check_output(['where', 'adb']).decode('utf-8').strip().splitlines()[0]
+            return adb_path
+        if os.environ.get('ANDROID_HOME'):
+            adb_path = os.path.join(os.environ['ANDROID_HOME'], 'platform-tools', 'adb.exe')
+            if os.path.exists(adb_path):
+                return adb_path
+    else:  # Assuming a Unix-like system
+        if subprocess.call(['which', 'adb'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0:
+            adb_path = subprocess.check_output(['which', 'adb']).decode('utf-8').strip()
+            return adb_path
+        if os.environ.get('ANDROID_HOME'):
+            adb_path = os.path.join(os.environ['ANDROID_HOME'], 'platform-tools', 'adb')
+            if os.path.exists(adb_path):
+                return adb_path
+    
+    return ''
+
+
+def find_device_serial(adb: str) -> str:
+    """
+    Find the device serial number.
+    This function should be implemented to retrieve the device serial number.
+    """
+    print(f"Finding device serial using adb: {adb}")
+    try:
+        output = subprocess.check_output([adb, 'devices']).decode('utf-8')
+        for line in output.splitlines():
+            if '\tdevice' in line:
+                return line.split('\t')[0]
+    except subprocess.CalledProcessError as e:
+        print(f"Error finding device serial: {e}")
+    return ''
+
+
+if __name__ == '__main__':
+    main()
